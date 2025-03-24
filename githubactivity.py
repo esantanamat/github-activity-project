e8e04c13839d1f91287411cac1979c3e3d72bb76
esantanamat <esantanamat@gmail.com>
2025-03-24 13:57:40 -0400

    commits and creates user activity

---
+++ githubactivity.py
@@ -0,0 +1,35 @@
+import urllib.request
+import json
+import datetime
+import argparse
+
+def get_activity(username):
+    url = f"https://api.github.com/users/{username}/events"
+    response = urllib.request.urlopen(url)
+    data = json.load(response)
+    today = datetime.datetime.now(datetime.timezone.utc).date()
+    print(today)
+    start_of_week = today - datetime.timedelta(days=today.weekday())
+    commits = 0
+    creates = 0
+    for push in data:
+        event_date = datetime.datetime.strptime(push["created_at"], "%Y-%m-%dT%H:%M:%SZ").date()
+        if push["type"] == "PushEvent":
+            if start_of_week <= event_date <= today:
+                commits +=1
+        elif push["type"] == "CreateEvent":
+            if start_of_week <= event_date <= today:
+                creates +=1 
+    print(f"there are {commits} commits this week")
+    print(f"There has been {creates} created repos this week")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Github activity tracker")
+    subparsers = parser.add_subparsers(dest="command")
+
+    get_activity_parser = subparsers.add_parser("get-activity", help="Retreive users activity")
+    get_activity_parser.add_argument("username", help="Username of user to fetch activity")
+    args = parser.parse_args()
+    if args.command == "get-activity":
+        get_activity(args.username)
